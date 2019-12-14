5b257878790b50fa77963ecdfb1611512e3766f4
Christian Preda <christian_preda@msn.com>
2019-12-14 17:48:36 +0100

    CRUD d ouf

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,24 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 #lire toutes les tasks
-get "tasks" => "tasks#index"
-
-#lire une task
-get "tasks/:id" => "tasks#show"
+get "tasks", to: "tasks#index", as: :tasks
 
 #creer une task (2 requets)
-get "tasks/new" => "tasks#new"
+get "tasks/new" => "tasks#new", as: :new_task
 post "tasks" => "tasks#create"
 
 #updater une task(2 requets)
-get "tasks/:id/edit" => "tasks#edit"
+get "tasks/:id/edit" => "tasks#edit", as: :edit
 patch "tasks/:id" => "tasks#update"
 
+#lire une task
+get "tasks/:id" => "tasks#show", as: :task
+
 #supprimer une task
-delete "tasks/:id" => "tasks#destroy"
+delete "tasks/:id" => "tasks#destroy", as: :destroy
 end
+
+
+
+
+
