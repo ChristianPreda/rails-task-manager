91a397b4e7381cacaad1285de8a126b2e9b1aaca
Christian Preda <christian_preda@msn.com>
2019-12-14 14:17:42 +0100

    add validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#lire toutes les tasks
+get "tasks" => "tasks#index"
+
+#lire une task
+get "tasks/:id" => "tasks#show"
+
+#creer une task (2 requets)
+get "tasks/new" => "tasks#new"
+post "tasks" => "tasks#create"
+
+#updater une task(2 requets)
+get "tasks/:id/edit" => "tasks#edit"
+patch "tasks/:id" => "tasks#update"
+
+#supprimer une task
+delete "tasks/:id" => "tasks#destroy"
 end
